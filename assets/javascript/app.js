eb2a8254def1dd3a640574d6e15867dd32b1de94
MichaelH94 <michaelhaggard94@gmail.com>
2018-09-27 20:19:21 -0500

    semi functional last fm api pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,54 @@
-// var config = {
-   // apiKey: "AIzaSyDgZnzLX0LLIVRKIPRnQZGxASVr-0g5-A4",
- //   authDomain: "kuproject1-2bd9a.firebaseapp.com",
- //   databaseURL: "https://kuproject1-2bd9a.firebaseio.com",
- //   projectId: "kuproject1-2bd9a",
- //   storageBucket: "kuproject1-2bd9a.appspot.com",
- //   messagingSenderId: "641399126415"
-//  };
+$(document).ready(function() {
 
- // firebase.initializeApp(config);
 
-//  var db = firebase.database();
-  
+var config = {
+   apiKey: "AIzaSyDgZnzLX0LLIVRKIPRnQZGxASVr-0g5-A4",
+   authDomain: "kuproject1-2bd9a.firebaseapp.com",
+   databaseURL: "https://kuproject1-2bd9a.firebaseio.com",
+   projectId: "kuproject1-2bd9a",
+   storageBucket: "kuproject1-2bd9a.appspot.com",
+   messagingSenderId: "641399126415"
+  };
 
+ firebase.initializeApp(config);
+ 
+ var db = firebase.database();
+
+
+function extraLargeImage(element) {
+    return element.size === 'extralarge';
+}
+
+var artistResult = ["", "", ""];
+var artistResultImage = ["", "", ""];
+var artistResultLastFM = ["", "", ""];
+var searchRdy = false;
 
  $("#submitbtn").click(function(e) {
     e.preventDefault();
+
+    lastFMPull();
+
+    for(x=0; x < 3; x++) {
+            createCard(artistResultImage[x], artistResult[x], artistResultLastFM[x]);
+            }
+
+function createCard(aIMG, aName, aLFM) {
+    $("#displayResults").append(
+        '<div class="card">'  +
+        '<img class="card-img-top" src="' + aIMG +
+        '">' + '<div class="card-body">' +
+        '<h5 class="card-title">' + aName +'</h5>' +
+        '<a href="' + aLFM + '" class="card-link">' + aName +
+        ' on Last.fm' + '</a>' +
+        '</div>'
+    );
+
+}
+
+function lastFMPull() {
+
+console.log(artistResult[0], artistResult[1],artistResult[2])
     var api = "5ab1615116e0cf8fa1c5270f3ab5310b";
     var artist = $("#searchForm").val();
     var artistSearch = artist.split(' ').join('+');
@@ -25,17 +59,23 @@
     method: "GET"
 }).done(function(response) {
 
-
     for(x = 0; x < 3; x++) {
     var artistName = response.similarartists.artist[x].name;
     var artistLastFM = response.similarartists.artist[x].url;
-    var artistImage = response.similarartists.artist[x].url;
-    
-    console.log(artistName);
-    $("#test").append(artistName)
-    }
+    var artistImage = response.similarartists.artist[x].image.find(extraLargeImage);
 
+    artistImage = artistImage ? artistImage['#text'] : '';
     
+    artistResult[x] = artistName;
+    artistResultImage[x] = artistImage;
+    artistResultLastFM[x] = artistLastFM;
+
+        }
+    });
+
+    return true;
+}
+
+    });
+
 });
-
- });
